563e47d3b1bb374caa10258e892932bc5d8a78f7
Stephane Lafontaine <stephane@medqigong.ca>
2024-05-11 14:39:25 -0400

    Started scraping services WIP

--- app/services/test_call.rb
+++ app/services/test_call.rb
@@ -2,37 +2,46 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'json'
 
 def fetch_nokogiri_doc(url)
   html = URI.open(url).read
   Nokogiri::HTML.parse(html)
 end
 
-def fetch_areas_links(html_doc)
-  areas = '.lef-nav-row'
-
-  main_html_doc.search(areas).map do |element|
-    link = element.children[1].attribute('href').value
-    # name = element.children[1].text.match(/(\(.+\)) (?<name>.+)/)[:name].strip
-    # { name => link }
-  end
-end
-
-def scrape_area(link)
+def scrape(link, selector)
   html_doc = fetch_nokogiri_doc(link)
 
-  areas = '.lef-nav-row'
 
-  main_html_doc.search(areas).map do |element|
-    link = element.children[1].attribute('href').value
-    # name = element.children[1].text.match(/(\(.+\)) (?<name>.+)/)[:name].strip
-    # { name => link }
+  html_doc.search(areas).map do |element|
+    name = element.children.text.strip
+    link = element.children.attribute('href').value
+    { name => link }
   end
 end
 
-url = 'https://www.mountainproject.com/area/105946462/skaha'
+base_url = 'https://www.thecrag.com/'
+filepath = 'db/scraped_data/skaha_bronze.json'
+selector = 'div.route'
+pp sector_urls = JSON.parse(File.read(filepath))
 
-html_doc = fetch_nokogiri_doc(url)
-areas_links = fetch_areas_links(html_doc)
+# routes = sector_urls.first(1).each_with_object({}) do |link_hash, result_hash|
+#                                 area_name = link_hash.keys.first
+#                                 area_links = scrape(base_url + link_hash.values.first, selector)
+#                                 result_hash[area_name] = area_links
+#                               end
 
-pp areas_data = area_links.first(1).map { |link| scrape_area(link) }
+# url = 'https://www.thecrag.com/en/climbing/canada/skaha'
+# selector = '.selected.open-submenu > ul li'
+# areas_links = scrape(url, selector)
+
+# File.open('db/scraped_data/skaha_bronze.json', "wb") do |file|
+#   file.write(JSON.pretty_generate(areas_links))
+# end
+
+# sector_data = areas_links.each_with_object({}) do |link_hash, result_hash|
+#                 area_name = link_hash.keys.first
+#                 area_links = scrape(link_hash.values.first)
+#                 result_hash[area_name] = area_links
+#               end
+# pp sector_data
